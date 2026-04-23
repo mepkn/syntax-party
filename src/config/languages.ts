61b6f6a58c51bf19a057b7d99ed4a5ea3d26a828
Praveen Kumar Nirmalkar <iamthepkn@gmail.com>
2026-04-23 11:12:02 +0530

    feat: add language configuration for JavaScript, Python, and Go with associated metadata

---
+++ src/config/languages.ts
@@ -0,0 +1,42 @@
+export const LANGUAGE_IDS = ['javascript', 'python', 'go'] as const
+export type LanguageId = typeof LANGUAGE_IDS[number]
+
+export interface Language {
+  id: LanguageId
+  name: string
+  shikiLang: string
+  iconPath: string
+  color: string
+  docsUrl: string
+  order: number
+}
+
+export const LANGUAGES: Record<LanguageId, Language> = {
+  javascript: {
+    id: 'javascript',
+    name: 'JavaScript',
+    shikiLang: 'javascript',
+    iconPath: '/icons/javascript.svg',
+    color: '#F7DF1E',
+    docsUrl: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+    order: 1,
+  },
+  python: {
+    id: 'python',
+    name: 'Python',
+    shikiLang: 'python',
+    iconPath: '/icons/python.svg',
+    color: '#3776AB',
+    docsUrl: 'https://docs.python.org/3/',
+    order: 2,
+  },
+  go: {
+    id: 'go',
+    name: 'Go',
+    shikiLang: 'go',
+    iconPath: '/icons/go.svg',
+    color: '#00ADD8',
+    docsUrl: 'https://go.dev/doc/',
+    order: 3,
+  },
+}
